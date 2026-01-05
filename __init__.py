e399cb6e4154b4aa1e3dd94f66fceaeb2ed83404
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2026-01-05 16:10:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,6 +38,23 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
+@app.route("/commits-data/")
+def commits_data():
+    url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
+    response = urlopen(url)
+    raw = response.read()
+    data = json.loads(raw.decode("utf-8"))
+
+    results = []
+
+    for commit in data:
+        date_string = commit.get("commit", {}).get("author", {}).get("date")
+        if date_string:
+            date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+            results.append(date_object.minute)
+
+    return jsonify(results=results)
+
 @app.route("/commits/")
 def commits():
     return render_template("commits.html")
