5eda13deb421aa433b52f991ee59d60d871ed683
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2026-01-05 15:11:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #bien?
+    return render_template('hello.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
@@ -28,3 +28,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
